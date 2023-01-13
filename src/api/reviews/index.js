be168b3546fa6a326edb0dcb33e1cd43a9532cf7
Nic <nic.dan.costea@gmail.com>
2023-01-13 19:53:41 +0100

    more CRUD

--- src/api/reviews/index.js
+++ src/api/reviews/index.js
@@ -21,13 +21,13 @@ reviewsRouter.post("/:mediaID/reviews", checksReviewSchema, triggerBadRequest, a
   try {
     const { mediaID } = req.params;
     const mediasArray = await getMedias();
-
     const searchedMedia = mediasArray.find((media) => media.id === mediaID);
 
     if (searchedMedia) {
       const newReview = {
         ...req.body,
         _id: uinqid(),
+        elementID: searchedMedia.id,
         createdAt: new Date(),
       };
 
@@ -46,15 +46,76 @@ reviewsRouter.post("/:mediaID/reviews", checksReviewSchema, triggerBadRequest, a
 
 // 2. GET all reviews for a single media
 reviewsRouter.get("/:mediaID/reviews", async (req, res, next) => {
-  console.log("req.body for GET all reviews: ", req.body);
   try {
+    const { mediaID } = req.params;
+    const mediasArray = await getMedias();
+    const searchedMedia = mediasArray.find((media) => media.id === mediaID);
+
+    if (searchedMedia) {
+      const reviewsArray = await getReviews();
+
+      if (req.query & req.query.search) {
+        const filteredReviews = reviewsArray.filter((review) => review.rate === req.query.rate);
+        res.send(filteredReviews);
+      } else {
+        res.send(reviewsArray);
+      }
+    } else {
+      next(NotFound(`Media with id: ${mediaID} not in our archive`));
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+// 3. GET single review for single media
+reviewsRouter.get("/:mediaID/reviews/:reviewID", async (req, res, next) => {
+  try {
+    const { mediaID } = req.params;
+    const { reviewID } = req.params;
+
     const mediaArray = await getMedias();
 
-    if (req.query && req.query.category) {
-      const filteredMedias = reviewsArray.filter((media) => media.category === req.query.category);
-      res.send(filteredMedias);
+    const searchedMedia = mediaArray.filter((media) => media.id === mediaID);
+
+    if (searchedMedia) {
+      const reviewsArray = await getReviews();
+      const searchedReview = reviewsArray.find((review) => review._id === reviewID);
+
+      if (searchedReview) {
+        res.send(searchedReview);
+      } else {
+        next(NotFound(`Review with id: ${reviewID} not in our archive`));
+      }
     } else {
-      res.send(reviewsArray);
+      next(NotFound(`Media with id: ${mediaID} not in our archive`));
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+// 4. PUT review
+reviewsRouter.put("/:mediaID/reviews/:reviewID", async (req, res, next) => {
+  try {
+    const { mediaID } = req.params;
+    const { reviewID } = req.params;
+
+    const mediaArray = await getMedias();
+
+    const searchedMedia = mediaArray.filter((media) => media.id === mediaID);
+
+    if (searchedMedia) {
+      const reviewsArray = await getReviews();
+      const searchedReview = reviewsArray.find((review) => review._id === reviewID);
+
+      if (searchedReview) {
+        res.send(searchedReview);
+      } else {
+        next(NotFound(`Review with id: ${reviewID} not in our archive`));
+      }
+    } else {
+      next(NotFound(`Media with id: ${mediaID} not in our archive`));
     }
   } catch (error) {
     next(error);
